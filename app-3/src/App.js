208f7cda98e58b6eccd97f8513489e513f6e66a3
jstntrn <justin.tran2290@gmail.com>
2019-01-14 11:51:19 -0700

    app-3 completed

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      futureGarage: ['Lamborghini Gallardo', 'Porsche GT2RS', 'Ford Raptor', 'BMW M3', 'Toyota Chaser'],
+      filteredGarage: ['Lamborghini Gallardo', 'Porsche GT2RS', 'Ford Raptor', 'BMW M3', 'Toyota Chaser'],
+      userInput: ''
+    }
+  }
+
+  handleInput(val){
+    var filteredArray = this.state.futureGarage.filter((element, i) => element.includes(val))
+    console.log(filteredArray)
+    this.setState({
+      userInput: val,
+      filteredGarage: filteredArray
+    })
+  }
+
   render() {
+    const displayList = this.state.filteredGarage.map((element, i) => (
+      <div>
+        <h2>{element}</h2>
+      </div>
+    ))
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input 
+          onChange = {(e) => this.handleInput(e.target.value)}
+          value = {this.state.userInput}/>
+        {displayList}
       </div>
     );
   }
